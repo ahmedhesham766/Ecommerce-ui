4fa4fabd431ec82ce8bae6a13443e9b9f95b2147
AhmedHm <ahmed.hesham@datagearbi.com>
2024-05-20 11:08:52 +0300

    againn

---
+++ src/app/category/catgory.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CategoriesComponent } from './components/categories/categories.component';
+
+
+
+
+@NgModule({
+  declarations: [
+    CategoriesComponent
+  ],
+  imports: [
+    CommonModule,
+    
+  ],
+  exports:[
+    CategoriesComponent
+  ]
+})
+export class CatgoryModule { }
